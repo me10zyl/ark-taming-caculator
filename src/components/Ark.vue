575a5cb8ddca8fa7a278be5120f55d025a0e089c
zyl <me10zyl@qq.com>
2019-07-08 16:17:24 +0800

    first commit

---
+++ src/components/Ark.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="main">
+        <div>
+            <div class="title">
+                生物详情
+            </div>
+            <div class="row">
+                <select>
+                    <option>官服毁灭服(3倍)</option>
+                    <option>官服(1倍)</option>
+                </select>
+            </div>
+            <div class="row">
+                生物：<input />
+            </div>
+            <div class="row">
+                等级：<input />
+            </div>
+        </div>
+        <div>
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ark",
+        props : {
+
+        }
+    }
+</script>
+
+<style scoped>
+    .row{
+
+    }
+</style>
