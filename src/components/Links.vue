b60331bf1f04256225ca051499fcf80050e08036
zyl <me10zyl@qq.com>
2020-04-26 17:22:33 +0800

    yeahhhhh

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,9 +1,18 @@
 <template>
 	<div>
 		<ol>
-			<li><a href="https://steamcommunity.com/sharedfiles/filedetails/?id=731604991" target="_blank">S+ steam创意工坊介绍</a></li>
-			<li><a href="https://steamcommunity.com/sharedfiles/filedetails/?id=731604991" target="_blank">Awesome Teleporters steam创意工坊介绍</a></li>
-			<li><a href="https://ark.gamepedia.com/Mod:Structures_Plus" target="_blank">S+ Wiki</a></li>
+			<li><span class="material-icons">
+favorite
+</span> <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=731604991" target="_blank">S+ steam创意工坊介绍</a></li>
+			<li><span class="material-icons">
+favorite
+</span> <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=889745138" target="_blank">Awesome Teleporters steam创意工坊介绍</a></li>
+			<li><span class="material-icons">
+favorite
+</span> <a href="https://ark.gamepedia.com/Mod:Structures_Plus" target="_blank">S+ Wiki</a></li>
+			<li><span class="material-icons">
+favorite
+</span> <a href="https://pan.baidu.com/s/13JUMp4ANEbksKnJbAJCBQg" target="_blank">视频技巧，方舟工具等。链接:https://pan.baidu.com/s/13JUMp4ANEbksKnJbAJCBQg  密码:s3s7</a></li>
 		</ol>
 	</div>
 </template>
